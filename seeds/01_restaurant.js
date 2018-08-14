046796ba430184d6c6a12cddcb17f095cb20fcdd
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-08-14 10:40:09 -0600

    first table finished

--- seeds/01_restaurant.js
+++ seeds/01_restaurant.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('restaurant').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex('restaurant').insert([
+        {id: 1, name: 'Pizza Hut', genre: 'Italian', address: 'addy1'},
+        {id: 2, name: 'McDonalds', genre: 'American', address: 'addy2'},
+        {id: 3, name: 'Taco Bell', genre: 'Mexican', address: 'addy3'},
+        {id: 4, name: 'Fazolis', genre: 'Italian', address: 'addy4'},
+        {id: 5, name: 'Piccolos', genre: 'Italian', 'Mexican', address: 'addy4'},
+      ])
+    })
+}
